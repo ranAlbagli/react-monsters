1c73749eb7c3cf1027d84afbedebd6082868983b
Ran  albagli <albagli.ran@gmail.com>
2019-09-19 19:32:54 +0300

    Updates

---
+++ precache-manifest.41d56bd029ccabd8036beca79a88d98b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a175bf856d0c28d98d9e9beddb12080",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "5a02fa898be16c056ff7",
+    "url": "/react-monsters/static/css/main.25ffe2bf.chunk.css"
+  },
+  {
+    "revision": "461f1073e4b33ee4a55b",
+    "url": "/react-monsters/static/js/2.c1642287.chunk.js"
+  },
+  {
+    "revision": "5a02fa898be16c056ff7",
+    "url": "/react-monsters/static/js/main.d3ba6f36.chunk.js"
+  },
+  {
+    "revision": "473abd285bc72c6aec58",
+    "url": "/react-monsters/static/js/runtime~main.11b3f9aa.js"
+  }
+]);
